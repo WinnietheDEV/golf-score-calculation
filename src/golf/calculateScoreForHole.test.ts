b9947d1b1a2374cb85b69741d82f9b5529258d0b
nawin <nawin.dev.th@gmail.com>
2025-05-30 20:51:05 +0700

    style: แก้การเว้นวรรใน calculateScoreForHole.test.ts

--- src/golf/calculateScoreForHole.test.ts
+++ src/golf/calculateScoreForHole.test.ts
@@ -2,86 +2,86 @@ import { calculateScoreForHole } from "./calculateScoreForHole";
 
 describe("calculateScoreForHole", () => {
   it("คืนค่า -1 เมื่อ par เป็น 3; minPar เป็น 1; totalStroke เป็น 2", () => {
-    //Arrange
+    // Arrange
     const par = 3;
     const minPar = 1;
     const totalStroke = 2;
     const expected = -1;
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 2 เมื่อ par เป็น 5; minPar เป็น 2; totalStroke เป็น 7", () => {
-    //Arrange
+    // Arrange
     const par = 5;
     const minPar = 2;
     const totalStroke = 7;
     const expected = 2;
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 0 เมื่อ par เป็น 4; minPar เป็น 1; totalStroke เป็น 4", () => {
-    //Arrange
+    // Arrange
     const par = 4;
     const minPar = 1;
     const totalStroke = 4;
     const expected = 0;
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 'Par must be greater than minPar' เมื่อ par เป็น 2; minPar เป็น 2; totalStroke เป็น 1", () => {
-    //Arrange
+    // Arrange
     const par = 2;
     const minPar = 2;
     const totalStroke = 1;
     const expected = "Par must be greater than minPar";
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 'MinPar must be greater than 0' เมื่อ par เป็น 3; minPar เป็น 0; totalStroke เป็น 2", () => {
-    //Arrange
+    // Arrange
     const par = 3;
     const minPar = 0;
     const totalStroke = 2;
     const expected = "MinPar must be greater than 0";
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 'Total stroke must be greater than or equal to minPar' เมื่อ par เป็น 5; minPar เป็น 3; totalStroke เป็น 2", () => {
-    //Arrange
+    // Arrange
     const par = 5;
     const minPar = 3;
     const totalStroke = 2;
     const expected = "Total stroke must be greater than or equal to minPar";
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 
   it("คืนค่า 'Par can be greater than minPar atmost 3' เมื่อ par เป็น 5; minPar เป็น 1; totalStroke เป็น 3", () => {
-    //Arrange
+    // Arrange
     const par = 5;
     const minPar = 1;
     const totalStroke = 3;
     const expected = "Par can be greater than minPar atmost 3";
-    //Act
+    // Act
     const result = calculateScoreForHole({ par, minPar, totalStroke });
-    //Assert
+    // Assert
     expect(result).toBe(expected);
   });
 });
