3e69ceacd2828296797734e331fa1452dfa07dba
nawin <nawin.dev.th@gmail.com>
2025-05-30 16:40:35 +0700

    feat: เขียนฟังก์ชั่นสำหรับ formatScore ต่อ 1 หลุม test: เขียน unit tests สำหรับฟังก์ชั่น formatScore

---
+++ src/golf/formatScore.test.ts
@@ -0,0 +1,111 @@
+import { formatHoleScore, SCORE_LABEL } from "./formatScore";
+
+describe("formatScore", () => {
+  it("คืนค่า {score: -3, label: 'Albatross'} ถ้า holeScore เป็น -3", () => {
+    // Arrange
+    const expected = { score: -3, label: SCORE_LABEL.ALBATROSS };
+    // Act
+    const result = formatHoleScore(-3);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: -2, label: 'Eagle'} ถ้า holeScore เป็น -2", () => {
+    // Arrange
+    const expected = { score: -2, label: SCORE_LABEL.EAGLE };
+    // Act
+    const result = formatHoleScore(-2);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: -1, label: 'Birdie'} ถ้า holeScore เป็น -1", () => {
+    // Arrange
+    const expected = { score: -1, label: SCORE_LABEL.BIRDIE };
+    // Act
+    const result = formatHoleScore(-1);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 0, label: 'Par'} ถ้า holeScore เป็น 0", () => {
+    // Arrange
+    const expected = { score: 0, label: SCORE_LABEL.PAR };
+    // Act
+    const result = formatHoleScore(0);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 1, label: 'Bogey'} ถ้า holeScore เป็น 1", () => {
+    // Arrange
+    const expected = { score: 1, label: SCORE_LABEL.BOGEY };
+    // Act
+    const result = formatHoleScore(1);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 2, label: 'Double Bogey'} ถ้า holeScore เป็น 2", () => {
+    // Arrange
+    const expected = { score: 2, label: SCORE_LABEL.DOUBLE_BOGEY };
+    // Act
+    const result = formatHoleScore(2);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 3, label: 'Triple Bogey'} ถ้า holeScore เป็น 3", () => {
+    // Arrange
+    const expected = { score: 3, label: SCORE_LABEL.TRIPLE_BOGEY };
+    // Act
+    const result = formatHoleScore(3);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 4, label: 'Quadruple Bogey'} ถ้า holeScore เป็น 4", () => {
+    // Arrange
+    const expected = { score: 4, label: SCORE_LABEL.QUADRUPLE_BOGEY };
+    // Act
+    const result = formatHoleScore(4);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 5, label: 'Quintuple Bogey'} ถ้า holeScore เป็น 5", () => {
+    // Arrange
+    const expected = { score: 5, label: SCORE_LABEL.QUINTUPLE_BOGEY };
+    // Act
+    const result = formatHoleScore(5);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 6, label: '%label'} ถ้า holeScore เป็น 6", () => {
+    // Arrange
+    const expected = { score: 6, label: "6" };
+    // Act
+    const result = formatHoleScore(6);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า {score: 15, label: '%label'} ถ้า holeScore เป็น 15", () => {
+    // Arrange
+    const expected = { score: 15, label: "15" };
+    // Act
+    const result = formatHoleScore(15);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it("คืนค่า 'Hole score must be greater than or equal to -3' ถ้า holeScore เป็น -4", () => {
+    // Arrange
+    const expected = "Hole score must be greater than or equal to -3";
+    // Act
+    const result = formatHoleScore(-4);
+    // Assert
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/golf/formatScore.ts
@@ -0,0 +1,44 @@
+export enum SCORE_LABEL {
+  ALBATROSS = "Albatross",
+  EAGLE = "Eagle",
+  BIRDIE = "Birdie",
+  PAR = "Par",
+  BOGEY = "Bogey",
+  DOUBLE_BOGEY = "Double Bogey",
+  TRIPLE_BOGEY = "Triple Bogey",
+  QUADRUPLE_BOGEY = "Quadruple Bogey",
+  QUINTUPLE_BOGEY = "Quintuple Bogey",
+}
+
+export function formatHoleScore(holeScore: number) {
+  if (holeScore < -3) {
+    return "Hole score must be greater than or equal to -3";
+  }
+
+  if (holeScore > 5) {
+    return { score: holeScore, label: holeScore.toString() };
+  }
+
+  switch (holeScore) {
+    case -3:
+      return { score: -3, label: SCORE_LABEL.ALBATROSS };
+    case -2:
+      return { score: -2, label: SCORE_LABEL.EAGLE };
+    case -1:
+      return { score: -1, label: SCORE_LABEL.BIRDIE };
+    case 0:
+      return { score: 0, label: SCORE_LABEL.PAR };
+    case 1:
+      return { score: 1, label: SCORE_LABEL.BOGEY };
+    case 2:
+      return { score: 2, label: SCORE_LABEL.DOUBLE_BOGEY };
+    case 3:
+      return { score: 3, label: SCORE_LABEL.TRIPLE_BOGEY };
+    case 4:
+      return { score: 4, label: SCORE_LABEL.QUADRUPLE_BOGEY };
+    case 5:
+      return { score: 5, label: SCORE_LABEL.QUINTUPLE_BOGEY };
+    default:
+      break;
+  }
+}
